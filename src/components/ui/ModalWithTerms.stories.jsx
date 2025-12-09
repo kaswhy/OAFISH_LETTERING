08e31006a1698d4c14cb50f010c8431ee0a87d15
kaswhy <sxshx818@naver.com>
2025-12-09 15:41:30 +0900

    refactor: Modal 결합 컴포넌트 스토리북 리팩토링

--- src/components/ui/ModalWithTerms.stories.jsx
+++ src/components/ui/ModalWithTerms.stories.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Modal from "./Modal";
 import TermsModalContent from "./modal-contents/TermsModalContent";
 
 export default {
   title: "UI/Modal/Composed/Terms",
-  component: TermsModalContent,
+  component: Modal, // <- 조합의 핵심 단위는 Modal
   tags: ["autodocs"],
   argTypes: {
     text: { control: "text" },
@@ -33,19 +33,19 @@ export const Interactive = {
 
 1. 수집 항목: 닉네임, 연락처(휴대폰 번호 또는 이메일)
 2. 수집 목적:
-\t- 이벤트 참여 확인 및 관리
-\t- 웹사이트를 통한 개인별 메시지 확인 서비스 제공
-\t- 결과 안내 및 관련 소통
-3. 보유·이용 기간: 이벤트 종료 및 결과 확인 가능 기간 종료 후 3개월 이내 파기
-4. 동의 거부 권리: 개인정보 제공을 거부할 수 있으며, 거부 시 이벤트 참여 및 결과 확인 서비스 이용이 제한됩니다.`,
+  - 이벤트 참여 확인 및 관리
+  - 웹사이트를 통한 개인별 메시지 확인 서비스 제공
+  - 결과 안내 및 관련 소통
+3. 보유·이용 기간: 이벤트 종료 후 3개월 이내 파기
+4. 동의 거부 권리: 거부 시 서비스 이용이 제한됩니다.`,
   },
-  render: (args) => {
-    const [open, setOpen] = useState(args.open);
-    useEffect(() => setOpen(args.open), [args.open]);
+
+  render: ({ open: initialOpen, text }) => {
+    const [open, setOpen] = useState(initialOpen);
 
     return (
       <Modal open={open} onClose={() => setOpen(false)}>
-        <TermsModalContent text={args.text} />
+        <TermsModalContent text={text} />
       </Modal>
     );
   },
