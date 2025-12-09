bc406d678f843756518c66d8f519e85d9f03fc03
kaswhy <sxshx818@naver.com>
2025-12-09 15:26:32 +0900

    refactor: SeedCard 리팩토링

--- src/components/ui/SeedCard.stories.jsx
+++ src/components/ui/SeedCard.stories.jsx
@@ -4,25 +4,36 @@ export default {
   title: "UI/SeedCard",
   component: SeedCard,
   tags: ["autodocs"],
+  args: {
+    type: "daisy",
+    selected: false,
+    size: "md",
+  },
   argTypes: {
     type: {
       control: "select",
       options: ["daisy", "rose", "freesia", "mugung", "susun", "sunflower"],
     },
+    size: {
+      control: "radio",
+      options: ["md", "lg"],
+    },
     onClick: { action: "clicked" },
   },
 };
 
-export const Default = {
-  args: {
-    type: "daisy",
-    selected: false,
-  },
-};
+export const Default = {};
 
 export const Selected = {
+  args: { selected: true },
+};
+
+export const Large = {
+  args: { size: "lg" },
+};
+
+export const Disabled = {
   args: {
-    type: "rose",
-    selected: true,
+    onClick: undefined,
   },
 };
