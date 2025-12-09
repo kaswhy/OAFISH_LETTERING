c0a779701bbd1b236c13a4f1be176fafe8c481bc
kaswhy <sxshx818@naver.com>
2025-12-09 15:38:23 +0900

    refactor: WishModalContent 리팩토링

--- src/components/ui/modal-contents/WishModalContent.stories.jsx
+++ src/components/ui/modal-contents/WishModalContent.stories.jsx
@@ -5,16 +5,24 @@ export default {
   component: WishModalContent,
   tags: ["autodocs"],
   args: {
+    type: "daisy",
     text: "저는 오피쉬의 정말 왕...\n줄바꿈 테스트",
     author: "오피쉬",
+    bodyHeight: 200,
   },
   argTypes: {
     type: {
       control: "select",
       options: ["daisy", "rose", "freesia", "mugung", "susun", "sunflower"],
     },
-    onClick: { action: "clicked" },
+    bodyHeight: { control: "number" },
   },
 };
 
 export const Default = {};
+
+export const LongText = {
+  args: {
+    text: "아주 긴 텍스트...\n".repeat(40),
+  },
+};
