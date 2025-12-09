1bfa2ffc5a611524d8681038f1d191cd8275f1a6
kaswhy <sxshx818@naver.com>
2025-12-09 15:35:12 +0900

    refactor: TermsModalContent 리팩토링

--- src/components/ui/modal-contents/TermsModalContent.stories.jsx
+++ src/components/ui/modal-contents/TermsModalContent.stories.jsx
@@ -9,12 +9,28 @@ export default {
 
 1. 수집 항목: 닉네임, 연락처(휴대폰 번호 또는 이메일)
 2. 수집 목적:
-\t- 이벤트 참여 확인 및 관리
-\t- 웹사이트를 통한 개인별 메시지 확인 서비스 제공
-\t- 결과 안내 및 관련 소통
+  - 이벤트 참여 확인 및 관리
+  - 웹사이트를 통한 개인별 메시지 확인 서비스 제공
+  - 결과 안내 및 관련 소통
 3. 보유·이용 기간: 이벤트 종료 및 결과 확인 가능 기간 종료 후 3개월 이내 파기
 4. 동의 거부 권리: 개인정보 제공을 거부할 수 있으며, 거부 시 이벤트 참여 및 결과 확인 서비스 이용이 제한됩니다.`,
+  },
+  argTypes: {
+    maxHeight: { control: "number" },
+    paddingTop: { control: "number" },
   },
 };
 
 export const Default = {};
+
+export const ShortText = {
+  args: {
+    text: "짧은 약관 내용입니다.",
+  },
+};
+
+export const LongText = {
+  args: {
+    text: "아주 긴 텍스트...\n".repeat(50),
+  },
+};
